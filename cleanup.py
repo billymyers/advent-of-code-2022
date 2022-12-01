1f0033c6e4dd6835b12a0146fbbf878cd0164253
billymyers <billymyers1020@gmail.com>
2022-11-30 17:06:57 -0800

    updates gitignore + cleanup script

---
+++ cleanup.py
@@ -0,0 +1,7 @@
+import os, re, os.path
+pattern = "^.*\.(exe|pdb)$"
+
+if __name__ == "__main__":
+    for r, subdirs, files in os.walk(os.getcwd()):
+        for file in filter(lambda x: re.match(pattern, x), files):
+            os.remove(os.path.join(r, file))
